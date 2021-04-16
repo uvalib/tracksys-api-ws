b69d935ea4528b4a20bfc570aede4f6ed6aa55c9
Lou Foster <lf6f@virginia.edu>
2021-04-16 11:11:12 -0400

    stub in pid summary for MD and MF. Component done

--- cmd/pid.go
+++ cmd/pid.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"net/http"
 	"strings"
@@ -9,10 +10,88 @@ import (
 	dbx "github.com/go-ozzo/ozzo-dbx"
 )
 
+type metadataSummary struct {
+	ID            int64          `db:"id"`
+	Type          string         `db:"type"`
+	Title         string         `db:"title"`
+	AvailbilityID sql.NullInt64  `db:"availability_policy_id"`
+	OCRLangHint   sql.NullString `db:"ocr_language_hint"`
+	OCRHint       sql.NullString `db:"ocr_hint"`
+	OCRCandidate  sql.NullBool   `db:"ocr_candidate"`
+}
+
+type masterFileSummary struct {
+	ID         int64          `db:"id"`
+	Title      sql.NullString `db:"title"`
+	Filename   sql.NullString `db:"filename"`
+	TextSource sql.NullString `db:"text_source"`
+	UnitID     int64          `db:"unit_id"`
+}
+
+type componentSummary struct {
+	ID    int64          `db:"id"`
+	Title sql.NullString `db:"title"`
+}
+
+type ocrSummary struct {
+	OCRHint          string `json:"ocr_hint,omitempty"`
+	OCRLanguageHint  string `json:"ocr_language_hint,omitempty"`
+	OCRCandidate     bool   `json:"ocr_candidate,omitempty"`
+	HasOCR           bool   `json:"has_ocr,omitempty"`
+	HasTranscription bool   `json:"has_transcription,omitempty"`
+}
+
+type pidSummary struct {
+	ID         int64      `json:"id"`
+	PID        string     `json:"pid"`
+	Type       string     `json:"type"`
+	Title      string     `json:"title,omitempty"`
+	Filename   string     `json:"filename,omitempty"`
+	ParentPID  string     `json:"parent_metadata_pid,omitempty"`
+	TextSource string     `json:"text_source,omitempty"`
+	ClonedFrom *cloneData `json:"cloned_from,omitempty"`
+	*ocrSummary
+}
+
 func (svc *ServiceContext) getPIDSummary(c *gin.Context) {
 	pid := c.Param("pid")
 	log.Printf("Get summary for %s", pid)
-	c.String(http.StatusNotImplemented, "not yet")
+
+	// First try metadata...
+	sql := `select m.id,type,title,availability_policy_id,ocr_language_hint,o.name as ocr_hint,ocr_candidate
+		from metadata m left outer join ocr_hints o on o.id = m.ocr_hint_id where pid={:pid}`
+	q := svc.DB.NewQuery(sql)
+	q.Bind(dbx.Params{"pid": pid})
+	var mdResp metadataSummary
+	err := q.One(&mdResp)
+	if err == nil {
+		log.Printf("METADATA: %+v", mdResp)
+		return
+	}
+
+	// try master file...
+	sql = `select id,title,filename,text_source,unit_id from master_files where pid={:pid}`
+	q = svc.DB.NewQuery(sql)
+	q.Bind(dbx.Params{"pid": pid})
+	var mfResp masterFileSummary
+	err = q.One(&mfResp)
+	if err == nil {
+		log.Printf("MASTEFILE: %+v", mfResp)
+		return
+	}
+
+	// try component...
+	q = svc.DB.NewQuery("select id,title from components where pid={:pid}")
+	q.Bind(dbx.Params{"pid": pid})
+	var cResp componentSummary
+	err = q.One(&cResp)
+	if err == nil {
+		out := pidSummary{ID: cResp.ID, PID: pid, Title: cResp.Title.String}
+		c.JSON(http.StatusOK, out)
+		return
+	}
+
+	c.String(http.StatusNotFound, "not found")
 }
 
 func (svc *ServiceContext) getPIDText(c *gin.Context) {
@@ -26,13 +105,13 @@ func (svc *ServiceContext) getPIDType(c *gin.Context) {
 	log.Printf("Get type for %s", pid)
 
 	// First try metadata
-	sql := `select type,e.name as ext_system from metadata m left outer join external_systems e
+	qSQL := `select type,e.name as ext_system from metadata m left outer join external_systems e
 		on e.id = m.external_system_id where pid={:pid}`
-	q := svc.DB.NewQuery(sql)
+	q := svc.DB.NewQuery(qSQL)
 	q.Bind(dbx.Params{"pid": pid})
 	var resp struct {
-		Type   string  `db:"type"`
-		System *string `db:"ext_system"`
+		Type   string         `db:"type"`
+		System sql.NullString `db:"ext_system"`
 	}
 	err := q.One(&resp)
 	if err == nil {
@@ -40,11 +119,11 @@ func (svc *ServiceContext) getPIDType(c *gin.Context) {
 			c.String(http.StatusOK, "sirsi_metadata")
 		} else if resp.Type == "XmlMetadata" {
 			c.String(http.StatusOK, "xml_metadata")
-		} else if resp.Type == "ExternalMetadata" && resp.System != nil && *resp.System == "ArchivesSpace" {
+		} else if resp.Type == "ExternalMetadata" && resp.System.String == "ArchivesSpace" {
 			c.String(http.StatusOK, "archivesspace_metadata")
-		} else if resp.Type == "ExternalMetadata" && resp.System != nil && *resp.System == "Apollo" {
+		} else if resp.Type == "ExternalMetadata" && resp.System.String == "Apollo" {
 			c.String(http.StatusOK, "apollo_metadata")
-		} else if resp.Type == "ExternalMetadata" && resp.System != nil && *resp.System == "JSTOR Forum" {
+		} else if resp.Type == "ExternalMetadata" && resp.System.String == "JSTOR Forum" {
 			c.String(http.StatusOK, "jstor_metadata")
 		} else {
 			c.String(http.StatusInternalServerError, "unknown metadata type")
